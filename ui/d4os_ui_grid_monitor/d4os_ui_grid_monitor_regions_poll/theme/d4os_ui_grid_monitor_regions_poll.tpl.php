afb19e04e89a867e0cb1cf95839dc953827465c2
ssm2017 <ssm2017.binder@gmail.com>
2010-09-29 01:29:20 +0200

    update : - regions poll (rewrited all, added batch, added cache)

--- ui/d4os_ui_grid_monitor/d4os_ui_grid_monitor_regions_poll/theme/d4os_ui_grid_monitor_regions_poll.tpl.php
+++ ui/d4os_ui_grid_monitor/d4os_ui_grid_monitor_regions_poll/theme/d4os_ui_grid_monitor_regions_poll.tpl.php
@@ -1,67 +1,151 @@
-<h1><?php print t("Grid regions VS Search regions"); ?></h1>
-<h2><?php print t("Initial count"); ?></h2>
-<p><?php print t("There are !regions regions in the grid.", array('!regions'=>$data['grid_regions_qty'])); ?></p>
-<p><?php print t("There are !regions regions in the search.", array('!regions'=>$data['search_regions_qty'])); ?></p>
-<h2><?php print t("Comparison"); ?></h2>
-<p><?php print t("There are !regions regions in the grid but not in the search", array('!regions'=>$data['diff_grid_vs_search_qty'])); ?></p>
-<p><?php print t("There are !regions regions in the search but not in the grid", array('!regions'=>$data['diff_search_vs_grid_qty'])); ?></p>
-<h2><?php print t("Grid regions analysis"); ?></h2>
-<p><?php print t("There are !hosts hosts.", array('!hosts'=>$data['hosts_qty'])); ?></p>
-<?php if (count($data['hosts'])): ?>
-<p><?php print t("In these hosts, there are !offline hosts not responding and !online hosts responding to /simstatus/", array('!offline'=>$data['offline_hosts_qty'], '!online'=>$data['online_hosts_qty'])); ?></p>
-<?php if (count($data['online_hosts'])): ?>
-<h3><?php print t("Online hosts"); ?></h3>
+<?php
+/**
+ * @package    d4os_ui_grid_monitor
+ * @subpackage regions_poll
+ * @copyright  Copyright (C) 2010 Wene - ssm2017 Binder ( S.Massiaux ). All rights reserved.
+ * @license    GNU/GPL, http://www.gnu.org/licenses/gpl-2.0.html
+ * D4os is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ */
+ 
+?>
+<style>
+  #regions_poll td {
+    padding:3px;
+    border:1px solid black;
+  }
+  #regions_poll td.green {
+    color:white;
+    background-color:green;
+  }
+  #regions_poll td.or {
+    color:white;
+    background-color:orange;
+  }
+  #regions_poll td.red {
+    color:white;
+    background-color:red;
+  }
+
+</style>
+<div id="regions_poll">
+<h2><?php print t("Hosts"); ?></h2>
+<h3><?php print t("Numbers"); ?></h3>
+<ul>
+  <li><?php print t("Machines"); ?> : <?php print count($data['hosts']['all']['ips']); ?></li>
+  <li><?php print t("In grid"); ?> : <?php print count($data['results']['hosts']['in_grid']); ?></li>
+  <li><?php print t("In search"); ?> : <?php print count($data['results']['hosts']['in_search']); ?></li>
+  <li><?php print t("Registered"); ?> : <?php print count($data['results']['hosts']['registered']); ?></li>
+  <li><?php print t("In grid but not in search"); ?> : <?php print count($data['results']['hosts']['in_grid_but_not_in_search']); ?></li>
+  <li><?php print t("In search but not in grid"); ?> : <?php print count($data['results']['hosts']['in_search_but_not_in_grid']); ?></li>
+  <li><?php print t("In search but not registered"); ?> : <?php print count($data['results']['hosts']['in_search_but_not_registered']); ?></li>
+  <li><?php print t("In all"); ?> : <?php print count($data['results']['hosts']['in_all']); ?></li>
+  <li><?php print t("Online"); ?> : <?php print count($data['results']['hosts']['online']); ?></li>
+  <li><?php print t("Online and in all"); ?> : <?php print count($data['results']['hosts']['online_in_all']); ?></li>
+  <li><?php print t("Online, in all, and collector"); ?> : <?php print count($data['results']['hosts']['online_in_all_and_collector']); ?></li>
+</ul>
+<h3><?php print t("Slow hosts"); ?></h3>
+<ul>
+  <?php foreach ($data['results']['hosts']['slow_items'] as $host): ?>
+  <li><?php print $host; ?></li>
+  <?php endforeach; ?>
+</ul>
+<h3><?php print t("All hosts list"); ?></h3>
 <table>
+  <caption><?php print t("Hosts status"); ?></caption>
   <thead>
-    <th><?php print t("collector ?"); ?></th>
-    <th><?php print t("Host Uri"); ?></th>
+    <th><?php print t("Machine IP"); ?></th>
+    <th><?php print t("Host url"); ?></th>
+    <th><?php print t("From grid"); ?></th>
+    <th><?php print t("In grid"); ?></th>
+    <th><?php print t("From search"); ?></th>
+    <th><?php print t("In search"); ?></th>
+    <th><?php print t("From registered"); ?></th>
+    <th><?php print t("Registered"); ?></th>
+    <th><?php print t("Is online"); ?></th>
+    <th><?php print t("Speed"); ?></th>
+    <th><?php print t("Use collector"); ?></th>
+    <th><?php print t("Speed"); ?></th>
     <th><?php print t("Region name"); ?></th>
+    <th><?php print t("Region uuid"); ?></th>
+    <th><?php print t("Region handle"); ?></th>
     <th><?php print t("Region owner"); ?></th>
+    <th><?php print t("In grid"); ?></th>
+    <th><?php print t("In search"); ?></th>
   </thead>
   <tbody>
-    <?php foreach ($data['online_hosts'] as $online_host): ?>
+    <?php foreach ($data['hosts']['all']['ips'] as $machine): ?>
     <tr>
-      <td><?php print ($online_host['collector']) ? "<strong>". t("Yes"). "</strong>": "&nbsp"; ?></td>
-      <td><?php print $online_host['host']; ?></td>
+      <td><?php print $machine; ?></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
       <td>&nbsp;</td>
       <td>&nbsp;</td>
     </tr>
-    <?php foreach ($online_host['regions'] as $region): ?>
+    <?php foreach ($data['hosts']['all']['items'] as $host): ?>
+    <?php if ($host['host_ip'] == $machine): ?>
+    <tr>
+      <td>&nbsp;</td>
+      <td><?php print $host['host_domain_url']; ?></td>
+      <td class="<?php print ($host['from_grid']) ? 'green': 'white';?>"><?php print ($host['from_grid']) ? t('Yes'): t('No');?></td>
+      <td class="<?php print ($host['in_grid']) ? 'green': 'white';?>"><?php print ($host['in_grid']) ? t('Yes'): t('No');?></td>
+      <td class="<?php print ($host['from_search']) ? 'green': 'white';?>"><?php print ($host['from_search']) ? t('Yes'): t('No');?></td>
+      <td class="<?php print ($host['in_search']) ? 'green': 'white';?>"><?php print ($host['in_search']) ? t('Yes'): t('No');?></td>
+      <td class="<?php print ($host['from_registered']) ? 'green': 'white';?>"><?php print ($host['from_registered']) ? t('Yes'): t('No');?></td>
+      <td class="<?php print ($host['registered']) ? 'green': 'white';?>"><?php print ($host['registered']) ? t('Yes'): t('No');?></td>
+      <td class="<?php print ($host['is_online']) ? 'green': 'white';?>"><?php print ($host['is_online']) ? t('Yes'): t('No');?></td>
+      <td class="<?php print $host['time']['simstatus']['color'];?>"><?php print round($host['time']['simstatus']['speed'], 3); ?></td>
+      <td class="<?php print ($host['use_collector']) ? 'green': 'white';?>"><?php print ($host['use_collector']) ? t('Yes'): t('No');?></td>
+      <td class="<?php print $host['time']['collector']['color'];?>"><?php print round($host['time']['collector']['speed'], 3); ?></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <?php foreach ($data['regions']['all']['items'] as $region): ?>
+    <?php if ($region['host_domain_url'] == $host['host_domain_url']): ?>
     <tr>
       <td>&nbsp;</td>
       <td>&nbsp;</td>
-      <td><?php print $region->regionName; ?></td>
-      <td><?php print $region->owner; ?></td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td><?php print $region['name']; ?></td>
+      <td><?php print $region['uuid']; ?></td>
+      <td><?php print $region['handle']; ?></td>
+      <td><?php print $region['owner_name']; ?></td>
+      <td class="<?php print ($region['in_grid']) ? 'green': 'white';?>"><?php print ($region['in_grid']) ? t('Yes'): t('No');?></td>
+      <td class="<?php print ($region['in_search']) ? 'green': 'white';?>"><?php print ($region['in_search']) ? t('Yes'): t('No');?></td>
     </tr>
+    <?php endif; // if ($region['host_domain_url'] == $host['host_domain_url']): ?>
     <?php endforeach; // region ?>
-    <?php endforeach; // online_host ?>
+    <?php endif; // if ($host->host_ip == $machine): ?>
+    <?php endforeach; // host ?>
+    <?php endforeach; // machine ?>
   </tbody>
 </table>
-<?php endif; // online_hosts ?>
-<?php if (count($data['offline_hosts'])): ?>
-<h3><?php print t("Offline hosts"); ?></h3>
-<table>
-  <thead>
-    <th><?php print t("Host Uri"); ?></th>
-    <th><?php print t("Region name"); ?></th>
-    <th><?php print t("Region owner"); ?></th>
-  </thead>
-  <tbody>
-    <?php foreach ($data['offline_hosts'] as $offline_host): ?>
-    <tr>
-      <td><?php print $offline_host['host']; ?></td>
-      <td>&nbsp;</td>
-      <td>&nbsp;</td>
-    </tr>
-    <?php foreach ($offline_host['regions'] as $region): ?>
-    <tr>
-      <td>&nbsp;</td>
-      <td><?php print $region->regionName; ?></td>
-      <td><?php print $region->owner; ?></td>
-    </tr>
-    <?php endforeach; // region ?>
-    <?php endforeach; // offline_host ?>
-  </tbody>
-</table>
-<?php endif; // offline_hosts ?>
-<?php endif; // hosts ?>
+</div>
